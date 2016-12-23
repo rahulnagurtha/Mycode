48b3fd1ef035e1f7b12bcb6681ea5cb63a8ced0e
rahulnagurtha <nagurtha.rahul@gmail.com>
2016-12-23 21:48:37 +0530

    CF 388 over

---
+++ codeforces/388-1.cpp
@@ -0,0 +1,91 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#ifndef ONLINE_JUDGE
+#include "../debug.hpp"
+struct debugger dbg;
+#else 
+#define debug(args...)              // Just strip off all debug tokens
+#endif
+
+#define si(i)                   scanf("%d",&i)
+#define si2(i,j)                scanf("%d %d",&i,&j)
+#define si3(i,j,k)              scanf("%d %d %d",&i,&j,&k)
+#define slli(i)                 scanf("%I64d",&i)
+#define slli2(i,j)              scanf("%I64d %I64d",&i,&j)
+#define slli3(i,j,k)            scanf("%I64d %I64d %I64d",&i,&j,&k)
+
+#define pi(i)                   printf("%d\n",i)
+#define plli(i)                 printf("%I64d\n",i)
+
+#define	forup(i,a,b) 			for(int i = (a); (i) < (b); ++(i))
+#define fordn(i,a,b) 			for(int i = (a); (i) > (b); --(i))
+#define rep(i,a) 				for(int i = 0; (i) < (a); ++(i))
+
+#define SYNC                    ios_base::sync_with_stdio(0)
+#define mp                      make_pair
+#define FF                      first
+#define SS                      second
+#define pb                      push_back
+#define fill(a,v)               memset(a,v,sizeof a)
+#define ceil(a,b)               (((a)%(b)==0)?((a)/(b)):((a)/(b)+1))
+#define rem(a,b)                ((a<0)?((((a)%(b))+(b))%(b)):((a)%(b)))
+#define MOD                     1000000007LL
+#define INF 					INT_MAX
+#define N                     	100007
+
+
+typedef long long int ll;
+typedef pair<int,int> PII;
+typedef pair<ll,ll> PLL;
+typedef vector<string> VS;
+typedef vector<int> VI;
+typedef vector<ll> VL;
+typedef vector<PII> VOII;
+typedef vector<PLL> VOLL;
+typedef vector<VI> VOVI;
+
+int dX[] = {-1,0,1,0,-1,1,1,-1};
+int dY[] = {0,1,0,-1,1,1,-1,-1};
+
+
+
+
+
+
+inline void Refresh() {
+    
+}
+
+
+int main()
+{
+	int n;
+	cin >> n;
+	if(n == 2) {
+		cout << "1" << endl;
+		cout << "2" << endl;
+	}
+	else if(n == 3) {
+		cout << "1" << endl;
+		cout << "3" << endl;
+	}
+	else {
+		VI ans;
+		if(n%2 == 1) {
+			ans.pb(3);
+			n -= 3;
+		}
+		while(n > 0) {
+			ans.pb(2);
+			n -= 2;
+		}
+		cout << ans.size() << endl;
+		for (int i = 0; i < ans.size(); ++i) {
+			cout << ans[i] << " ";
+		}
+		cout << endl;
+	}
+    return 0;
+}
